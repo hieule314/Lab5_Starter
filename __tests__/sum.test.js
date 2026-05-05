341773955f8ab4d960080bc24c6e16daaf00d46c
hieule314 <hil004@ucsd.edu>
2026-05-04 21:05:24 -0700

    master commit

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -2,4 +2,13 @@
 
 test('adds 1 + 2 to equal 3', () => {
   // TODO
+
+  test('adds 1 + 2 to equal 3', () => {
+  expect(1 + 2).toBe(4);
+});
+
+test('sum function adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(4);
+});
+
 });
