30e47bf9f7217d803f09947ec484ad1c202dbcd8
hieule314 <hil004@ucsd.edu>
2026-05-04 21:36:39 -0700

    fixed error

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -3,12 +3,9 @@
 test('adds 1 + 2 to equal 3', () => {
   // TODO
 
-  test('adds 1 + 2 to equal 3', () => {
   expect(1 + 2).toBe(3);
 });
 
 test('sum function adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
-});
-
 });
